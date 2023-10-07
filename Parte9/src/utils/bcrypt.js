013e08a868383bb7818968e27315229ceb2b7797
joselav <josefinalavinia05@gmail.com>
2023-10-07 15:01:07 +0200

    commit

--- Parte9/src/utils/bcrypt.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import bcrypt from 'bcrypt'
-
-const salt = parseInt(process.env.SALT)
-export const createHash = (password) => bcrypt.hashSync(password, salt)
-
-export const validatePassword = (passwordSend, passwordBDD) => bcrypt.compareSync(passwordSend, passwordBDD)
